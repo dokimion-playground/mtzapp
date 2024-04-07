2fb555aea94a83a8779a6ccccb25ae4d4eef69ec
dokimion24 <leecy3610@gmail.com>
2024-04-07 10:40:54 +0900

    feat: Add screen components

---
+++ front/src/screens/auth/SignupScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function SignupScreen() {
+  return (
+    <View>
+      <Text>SignupScreen</Text>
+    </View>
+  );
+}
+
+export default SignupScreen;
