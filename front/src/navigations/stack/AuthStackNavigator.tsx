4fa372aa4b4413d0d577d73beffaa44c13a73e58
dokimion24 <leecy3610@gmail.com>
2024-04-07 10:40:29 +0900

    feat: Add navigator

---
+++ front/src/navigations/stack/AuthStackNavigator.tsx
@@ -0,0 +1,46 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+import AuthHomeScreen from '../../screens/auth/AuthHomeScreen';
+import LoginScreen from '../../screens/auth/LoginScreen';
+import SignupScreen from '../../screens/auth/SignupScreen';
+import {authNavigations} from '../../constants';
+
+export type AuthStackParamList = {
+  [authNavigations.AUTH_HOME]: undefined;
+  [authNavigations.LOGIN]: undefined;
+  [authNavigations.SIGNUP]: undefined;
+};
+
+const Stack = createStackNavigator<AuthStackParamList>();
+
+function AuthStackNavigator() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        cardStyle: {backgroundColor: '#fff'},
+        headerStyle: {backgroundColor: '#fff', shadowColor: '#ccc'},
+        headerTitleStyle: {
+          fontSize: 15,
+        },
+        headerTintColor: '#000',
+      }}>
+      <Stack.Screen
+        name={authNavigations.AUTH_HOME}
+        component={AuthHomeScreen}
+        options={{headerTitle: '', headerShown: false}}
+      />
+      <Stack.Screen
+        name={authNavigations.LOGIN}
+        component={LoginScreen}
+        options={{headerTitle: '로그인'}}
+      />
+      <Stack.Screen
+        name={authNavigations.SIGNUP}
+        component={SignupScreen}
+        options={{headerTitle: '회원가입'}}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default AuthStackNavigator;
